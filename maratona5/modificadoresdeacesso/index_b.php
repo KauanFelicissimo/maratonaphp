37b529f59e925871f926c7a74b92d77c82692f5c
Kauan Souza Felicissimo <kauanfelicissimo@gmail.com>
2025-02-20 08:32:08 -0300

    maratona 5

--- maratona5/modificadoresdeacesso/index_b.php
+++ maratona5/modificadoresdeacesso/index_b.php
@@ -3,41 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Classe Produto Roupa</title>
 </head>
 <body>
     <?php 
-    class Roupas{
-        public $nome;
-        public $preco;
-        private $codigoproduto;
+    /*Crie uma classe chamada ProdutoRoupa com os seguintes atributos:
+• nome (String)
+• codigoProduto (String)
+• preco (Float)
+Implemente o modificador de acesso private para o atributo codigoProduto e crie 
+métodos get e set para permitir acessar e alterar o código do produto, mas 
+somente de forma controlada (por exemplo, o código do produto deve ser 
+validado antes de ser alterado). O atributo nome deve ser público e acessado 
+diretamente.
+Depois, crie um objeto dessa classe e:
+1. Tente acessar diretamente o codigoProduto (isso não deve ser possível).
+2. Use o método set para alterar o codigoProduto de forma controlada e o 
+método get para acessá-lo.
+*/
 
-        public function exibirDetalhes(){
-            echo "Nome: ". $this->nome . "<br>";
-            echo "Preço: ". number_format($this->preco, 2, ",", ".") . "<br>";
-           //echo "codigo do produto: ". $this->codigoproduto . "<br>"; 
-        }
-        public function setCodigoProduto($codigoproduto){
-            $this->codigoproduto = $codigoproduto;
-        }
-        public function getCodigoProduto(){
-            return $this->codigoproduto;
-        }
+class ProdutoRoupa {
+    public $nome;
+    private $codigoProduto;
+    public $preco;
+
+    public function __construct($nome, $codigoProduto, $preco) {
+        $this->nome = $nome;
+        $this->codigoProduto = $codigoProduto;
+        $this->preco = $preco;
     }
-    $roupa1 = new Roupas();
-    $roupa1->nome = "Camiseta";
-    $roupa1->preco = 50.00;
-    $roupa1->exibirDetalhes();
-    $roupa1->setCodigoProduto(123456);
-    echo "Código do produto: ". $roupa1->getCodigoProduto();
-    $roupa1->setCodigoProduto(654321);
-    // Valor após alteração
-    echo "<br>Código do produto: ". $roupa1->getCodigoProduto();
 
+    private function get_CodigoProduto() {
+        return $this->codigoProduto; // Retorna o valor
+    }
 
-  
-    
-    
+    public function set_CodigoProduto($codigoProduto) {
+        $this->codigoProduto = $codigoProduto; // Define o novo valor
+    }
+
+    public function mostrarCodigoProduto() {
+        echo "Código do produto: " . $this->codigoProduto;
+    }
+}
+
+$produto = new ProdutoRoupa("Camiseta", "12345", 29.90);
+
+echo "Nome do produto: " . $produto->nome;
+echo "<br>";
+echo "Preço do produto: R$ " . number_format($produto->preco, 2, ",", "."); // Formatação de preço
+echo "<br>";
+
+// $produto->codigoProduto; // Erro: acesso a atributo privado
+
+$produto->set_CodigoProduto("678900"); // Altera o código do produto
+
+$produto->mostrarCodigoProduto(); 
+
     ?>
 </body>
 </html>
